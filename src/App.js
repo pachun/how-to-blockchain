48e1735ddff0f757f8235e6cbb8b306a0cbdabdf
Nick Pachulski <nick@pachulski.me>
2018-01-25 19:26:15 -0500

    Add bootstrap using reactstrap
    http://reactstrap.github.io/

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Button color="danger">hello</Button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
